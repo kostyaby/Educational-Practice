14d3f54456d2a2c296cb408f9b3b0db13c9d01c0
Constantine Sokol <kostya.sokol.by@gmail.com>
2015-02-27 17:51:37 +0300

    Repository has been restructured

---
+++ JuiceSolver/src/Subtask.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public abstract class Subtask {
+
+    String fileName;
+
+    Subtask(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void solveSubtask(List<Juice> juices) {
+
+    }
+}
